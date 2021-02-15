73ab8a486733c8aa863322110716f66d62d27f0a
Bárbara Rodrigues <barbararodrigues633@gmail.com>
2021-02-15 12:17:24 -0300

    tela de cadastro plano conta

---
+++ src/app/area-logada/plano-conta/interfaces/plano-conta.interfaces.ts
@@ -0,0 +1,7 @@
+export interface PlanoConta {
+    id: number;
+    descricao: String;
+    login: String;
+    tipo_Lancamento: string;
+
+}
--- src/app/area-logada/plano-conta/plano-conta.component.ts
+++ src/app/area-logada/plano-conta/plano-conta.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-plano-conta',
@@ -6,17 +8,65 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ['./plano-conta.component.scss']
 })
 export class PlanoContaComponent implements OnInit {
+  modalReference: NgbModalRef | undefined;
+  mostrarErroData: boolean = false;
 
+  selTipoContaOperacao: string = "";
+  inpDescricao: string = "";
+  inpLogin: string = "";
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal
+  ) { }
 
   ngOnInit() {
     try {
 
     }
     catch (error) {
-      console.log(`Erro no método: ngOnInit.Dashboard: ${error}`);
+      console.log(`Erro no método: ngOnInit.PlanoConta: ${error}`);
     }
+  }
+
+  abrirModal(content: TemplateRef<NgbModalRef>) {
+    try {
+      this.modalReference = this.modalService.open(content, { centered: true, size: "sm", backdrop: "static" });
+    }
+    catch (error) {
+      console.log(`Erro no método: abrirModal.PlanoConta: ${error}`);
+    }
+  }
+
+
+  fecharModal() {
+    try {
+      this.mostrarErroData = false;
+      this.modalReference?.close();
+    }
+    catch (error) {
+      console.log(`Erro no método: fecharModal.PlanoConta: ${error}`);
+    }
+  }
+
+  save() {
+    try {
+    }
+    catch (error) {
+      console.log(`Erro no método: save.PlanoConta: ${error}`);
+    }
+  }
+
+  exibeErro(nomeControle: string, form: NgForm) {
+    try {
+      if (!form.controls[nomeControle]) {
+        return false;
+      }
+      return form.controls[nomeControle].invalid && form.controls[nomeControle].touched;
+    }
+    catch (error) {
+      console.log(`Erro no método: exibeErro.PlanoConta: ${error}`);
+    }
+    return false;
   }
 
 
